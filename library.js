24d256b56bcf655103f68927adee78c029cd58c3
William Steward <alexsteward98@gmail.com>
2021-01-02 16:19:24 -0600

    reorganzing code

--- library.js
+++ library.js
@@ -16,57 +16,10 @@ function Book(title, author, pages, isRead){
     this.isRead = isRead 
 }
 
-//adds the function info to the book object
-Book.prototype.info = function(){
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead? "completed":"not read yet"}`
-}
-
 //book collection
 let myLibrary = [];
 function addBookToLibary(book){
     myLibrary.push(book)
-}
-
-function displayBooks(){
-    const bookshelf = document.getElementById("bookshelf")
-    myLibrary.forEach(book => { 
-        bookshelf.appendChild(
-            htmlToElement(`
-                <div class="bookCard-wrapper">
-                    <div class="bookCard">    
-                        <h2>${book.title}</h2>
-                        <h3>${book.author}</h3>
-                        <p>${book.pages}</p>
-                        <p>${book.isRead ? "Completed" : "Incomplete"}</p>
-                    </div>
-                </div>`
-            )
-        )
-    })
-}
-
-
-for (let i = 0; i<10;i++){
-    addBookToLibary(new Book("Man and His Symbols", "Carl Jung", 300, true))
-}
-displayBooks()
-
-
-function hueShiftBooks(){
-    const books = document.getElementById("bookshelf").children
-    for(let book of books){
-        // console.log(book)
-        //hueshift the div containing the book background image
-        const hueShift = Math.floor(Math.random() *360) //random int from 0 to 360
-        book.setAttribute("style", `filter:hue-rotate(${hueShift}deg)`)
-        
-        //this is to de hueshift the contents of the book
-        for (let contents of book.children){
-            contents.setAttribute("style", `filter:hue-rotate(${-hueShift}deg)`)
-        }
-
-        // console.log(book)
-    }
 }
 
 const addBookButton = document.getElementById("addBookButton")
@@ -122,12 +75,66 @@ addBookButton.addEventListener('click', ()=>{
         }
 
         //add new book to library
-        addBookToLibary(new Book(...values))
+        let newBook = new Book(...values)
+        displayBook(newBook)
+        addBookToLibary(new Book)
+
+        addBookForm.remove()
     })
 
-
-
+    addBookForm.getElementsByClassName("cancel")[0].addEventListener('click', ()=>{
+        if (confirm("Cancel new book entry?")){
+            addBookForm.remove()
+        }
+    })
 
 })
+
+
+function displayBook(book){
+    const bookshelf = document.getElementById("bookshelf")
+    let newBook = htmlToElement(`
+            <div class="bookCard-wrapper">
+                <div class="bookCard">    
+                    <h2>${book.title}</h2>
+                    <h3>${book.author}</h3>
+                    <p>${book.pages}</p>
+                    <p>${book.isRead ? "Completed" : "Incomplete"}</p>
+                </div>
+            </div>`)
+    
+    addBookButton.insertAdjacentElement('afterend', newBook)
+
+}
+
+function displayLibrary(){
+    myLibrary.forEach(book => { 
+        displayBook(book)
+    })
+}
+
+
+for (let i = 0; i<20;i++){
+    addBookToLibary(new Book("Man and His Symbols", "Carl Jung", 300, true))
+}
+displayLibrary()
+
+
+function hueShiftBooks(){
+    const books = document.getElementById("bookshelf").children
+    for(let book of books){
+        // console.log(book)
+        //hueshift the div containing the book background image
+        const hueShift = Math.floor(Math.random() *360) //random int from 0 to 360
+        book.setAttribute("style", `filter:hue-rotate(${hueShift}deg)`)
+        
+        //this is to de hueshift the contents of the book
+        for (let contents of book.children){
+            contents.setAttribute("style", `filter:hue-rotate(${-hueShift}deg)`)
+        }
+
+        // console.log(book)
+    }
+}
 
 hueShiftBooks()
